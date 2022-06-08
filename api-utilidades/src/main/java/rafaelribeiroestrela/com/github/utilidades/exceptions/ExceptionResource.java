f579262533dab4e71572309de2284e5f9af928b6
Rafael Ribeiro Estrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2022-06-08 10:05:33 -0400

    Adicionado exceptions
    - EmptyResultDataAccessException
    - DataIntegrityViolationException

--- api-utilidades/src/main/java/rafaelribeiroestrela/com/github/utilidades/exceptions/ExceptionResource.java
+++ api-utilidades/src/main/java/rafaelribeiroestrela/com/github/utilidades/exceptions/ExceptionResource.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +16,26 @@ public class ExceptionResource {
 	
 	@ExceptionHandler(ApiException.class)
 	public ResponseEntity<StandardError> exceptionDefault(ApiException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		err.setTime(LocalDateTime.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	public ResponseEntity<StandardError> exceptionDefault(EmptyResultDataAccessException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		err.setTime(LocalDateTime.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> exceptionDefault(DataIntegrityViolationException e, HttpServletRequest request){
 		StandardError err = new StandardError();
 		err.setTime(LocalDateTime.now());
 		err.setStatus(HttpStatus.BAD_REQUEST.value());
